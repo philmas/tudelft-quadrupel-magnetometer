afc2fe39793cb03c88d1d30177a1c49633c6bc16
Philippe van Mastrigt <p.g.vanmastrigt@student.tudelft.nl>
2023-03-12 00:55:19 +0100

    upload

--- src/compass.rs
+++ src/compass.rs
@@ -28,7 +28,6 @@ struct Hmc5883l {
     x: i16,
     y: i16,
     z: i16,
-    last_measurement_time: Instant,
 }
 
 static MAGNETOMETER: Mutex<OnceCell<Hmc5883l>> = Mutex::new(OnceCell::uninitialized());
@@ -38,46 +37,43 @@ pub(crate) fn initialize() {
     let twi = unsafe { TWI.no_critical_section_lock_mut() };
 
     // twi.write(MAGNETOMETER_ADDR, REG_CR_A, &[]); // Send a "write" command to move the address pointer to register 0x00
-    twi.write(MAGNETOMETER_ADDR, REG_CR_A, &[0x70]); // 8 samples averaged, 15 Hz output
-    twi.write(MAGNETOMETER_ADDR, REG_CR_B, &[0x20]); // 1.3 gain
-    twi.write(MAGNETOMETER_ADDR, REG_MR, &[0x00]); // Continuous measurement mode
+    // twi.write(MAGNETOMETER_ADDR, REG_CR_A, &[0x70]); // 8 samples averaged, 15 Hz output
+    // twi.write(MAGNETOMETER_ADDR, REG_CR_B, &[0x20]); // 1.3 gain
+    // twi.write(MAGNETOMETER_ADDR, REG_MR, &[0x00]); // Continuous measurement mode
+
+    // Read the PROM values from the chip
 
     MAGNETOMETER.modify(|compass| {
-        compass.initialize(Hmc5883l {
-            x: 0,
-            y: 0,
-            z: 0,
-            last_measurement_time: Instant::now(),
-        });
+        compass.initialize(Hmc5883l { x: 0, y: 0, z: 0 });
     });
 }
 
-fn update() {
-    // Safety: The TWI and BAROMETER mutexes are not accessed in an interrupt
-    let twi = unsafe { TWI.no_critical_section_lock_mut() };
-    let compass = unsafe { MAGNETOMETER.no_critical_section_lock_mut() };
+// fn update() {
+//     // Safety: The TWI and BAROMETER mutexes are not accessed in an interrupt
+//     let twi = unsafe { TWI.no_critical_section_lock_mut() };
+//     let compass = unsafe { MAGNETOMETER.no_critical_section_lock_mut() };
 
-    let now = Instant::now();
+//     let now = Instant::now();
 
-    if now.duration_since(compass.last_measurement_time) < Duration::from_millis(100) {
-        return;
-    }
+//     if now.duration_since(compass.last_measurement_time) < Duration::from_millis(100) {
+//         return;
+//     }
 
-    compass.last_measurement_time = now;
+//     compass.last_measurement_time = now;
 
-    let mut data = [0u8; 6];
-    _ = twi.read(MAGNETOMETER_ADDR, REG_OUT_X_H, &mut data); // Read all 6 registers at once
+//     let mut data = [0u8; 6];
+//     _ = twi.read(MAGNETOMETER_ADDR, REG_OUT_X_H, &mut data); // Read all 6 registers at once
 
-    compass.x = ((data[0] as i16) << 8) | data[1] as i16;
-    compass.z = ((data[2] as i16) << 8) | data[3] as i16;
-    compass.y = ((data[4] as i16) << 8) | data[5] as i16;
-}
+//     compass.x = ((data[0] as i16) << 8) | data[1] as i16;
+//     compass.z = ((data[2] as i16) << 8) | data[3] as i16;
+//     compass.y = ((data[4] as i16) << 8) | data[5] as i16;
+// }
 
-/// Read the compass field in the x, y, and z directions.
-pub fn read_field() -> [i16; 3] {
-    update();
+// /// Read the compass field in the x, y, and z directions.
+// pub fn read_field() -> [i16; 3] {
+//     update();
 
-    let compass = unsafe { MAGNETOMETER.no_critical_section_lock_mut() };
+//     let compass = unsafe { MAGNETOMETER.no_critical_section_lock_mut() };
 
-    [compass.x, compass.y, compass.z]
-}
+//     [compass.x, compass.y, compass.z]
+// }
