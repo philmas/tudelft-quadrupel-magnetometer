c5740687f3aaa395ef68911f53c3b43f883b73b6
Philippe van Mastrigt <p.g.vanmastrigt@student.tudelft.nl>
2023-03-12 00:39:28 +0100

    added comment

--- src/compass.rs
+++ src/compass.rs
@@ -1,36 +1,36 @@
 use crate::mutex::Mutex;
 use crate::once_cell::OnceCell;
-use crate::time::Instant;
-use crate::twi::TWI;
-use core::time::Duration;
+// use crate::time::Instant;
+// use crate::twi::TWI;
+// use core::time::Duration;
 
-// Register Access
-const MAGNETOMETER_ADDR: u8 = 0x1E; // 0b0011110
+// // Register Access
+// const MAGNETOMETER_ADDR: u8 = 0x1E; // 0b0011110
 
-const REG_CR_A: u8 = 0x00; // Configuration register A
-const REG_CR_B: u8 = 0x01; // Configuration register B
-const REG_MR: u8 = 0x02; // Mode register
+// const REG_CR_A: u8 = 0x00; // Configuration register A
+// const REG_CR_B: u8 = 0x01; // Configuration register B
+// const REG_MR: u8 = 0x02; // Mode register
 
-//TODO Double check these
-const REG_OUT_X_H: u8 = 0x03; // X axis high byte
-const REG_OUT_X_L: u8 = 0x04; // X axis low byte
-const REG_OUT_Y_H: u8 = 0x05; // Y axis high byte
-const REG_OUT_Y_L: u8 = 0x06; // Y axis low byte
-const REG_OUT_Z_H: u8 = 0x07; // Z axis high byte
-const REG_OUT_Z_L: u8 = 0x08; // Z axis low byte
+// //TODO Double check these
+// const REG_OUT_X_H: u8 = 0x03; // X axis high byte
+// const REG_OUT_X_L: u8 = 0x04; // X axis low byte
+// const REG_OUT_Y_H: u8 = 0x05; // Y axis high byte
+// const REG_OUT_Y_L: u8 = 0x06; // Y axis low byte
+// const REG_OUT_Z_H: u8 = 0x07; // Z axis high byte
+// const REG_OUT_Z_L: u8 = 0x08; // Z axis low byte
 
-const REG_SR: u8 = 0x09; // Status register
-const ID_REG_A: u8 = 0x0A; // Identification register A
-const ID_REG_B: u8 = 0x0B; // Identification register B
-const ID_REG_C: u8 = 0x0C; // Identification register C
-
+// const REG_SR: u8 = 0x09; // Status register
+// const ID_REG_A: u8 = 0x0A; // Identification register A
+// const ID_REG_B: u8 = 0x0B; // Identification register B
+// const ID_REG_C: u8 = 0x0C; // Identification register C
+#[allow(dead_code)]
 struct Hmc5883l {
     x: i16,
     y: i16,
     z: i16,
 }
 
-static MAGNETOMETER: Mutex<OnceCell<Hmc5883l>> = Mutex::new(OnceCell::uninitialized());
+// static MAGNETOMETER: Mutex<OnceCell<Hmc5883l>> = Mutex::new(OnceCell::uninitialized());
 
 pub(crate) fn initialize() {
     // Safety: The TWI mutex are not accessed in an interrupt
